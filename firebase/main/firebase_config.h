e8e9b859df2bd70e9b2ceff66abe667b3bf2d662
pasidunethmina <nethminapasidu8@gmail.com>
2025-07-15 22:39:30 +0530

    files to upload the data to the firebase.

---
+++ firebase/main/firebase_config.h
@@ -0,0 +1,13 @@
+// Wifi Credentials
+#define SSID "Dialog 4G 406"
+#define PASSWORD "16bEc0c5"
+
+// Read readme.md to properly configure api key and authentication
+
+// create a new api key and add it here 
+#define API_KEY "sPQMqXuSbllWzTwzyF0mjJuIp9zm1VU4xgHdREH0"
+// Copy your firebase real time database link here 
+#define DATABASE_URL "https://esps3-6ac19-default-rtdb.asia-southeast1.firebasedatabase.app"  
+
+#define USER_EMAIL "nethminapasidu8@gmail.com"   // This gmail does not exist outside your database. it only exists in the firebase project as a user
+#define USER_PASSWORD "20020301"      // Dont add your gmail credentials. Setup users authentication in your Firebase project first
